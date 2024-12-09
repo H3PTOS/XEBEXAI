576070fe4b0b8fdd418a81420ae252077ccea693
Fake <147176149+H3PTOS@users.noreply.github.com>
2024-12-09 12:59:56 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Box, ClientOnly, Skeleton, VStack } from '@chakra-ui/react'
+import { ThemeToggle } from './components/theme-toggle'
+import Header from './components/header'
+import MainPage from './components/main-page'
+import { Toaster } from './components/ui/toaster'
+import Footer from './components/footer'
+import { Analytics } from '@vercel/analytics/react'
+
+export default function Page() {
+    return (
+        <Box paddingTop="24px">
+            <VStack>
+                <Box marginLeft="-16px">
+                    <Header/>
+                </Box>
+                <MainPage/>
+                <Box pos="absolute" top="4" right="4">
+                    <ClientOnly fallback={<Skeleton width="10" height="10" rounded="md"/>}>
+                        <ThemeToggle/>
+                    </ClientOnly>
+                </Box>
+                <Box bottom="4" paddingY="16px">
+                    <Footer/>
+                </Box>
+                <Toaster/>
+                <Analytics/>
+            </VStack>
+        </Box>
+    )
+}
